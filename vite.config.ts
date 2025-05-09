6ccfde61f1dafc343e7fce37eca19464a5a8bc18
tomzdon <tomekk.wlodarczyk@gmail.com>
2025-05-09 19:03:00 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     allowedHosts: [
       '8f33e939-79c4-4aaa-a7fb-0e38f6e273b5-00-3vkrajv9prt08.janeway.replit.dev',
-      'https://design-system-tomekkwlodarczy.replit.app/',
+      'design-system-tomekkwlodarczy.replit.app',
+      '*',
     ],
   },
 });
